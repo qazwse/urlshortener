0c0fbc6c952e4537dcdbe5754dae094cd6796412
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2019-08-23 10:42:28 -0400

    URL shortening in its own file, change file name

---
+++ urlshortener.go
@@ -0,0 +1,20 @@
+package main
+
+type shortURL struct {
+	Id           string
+	Url          string
+	Created      string
+	LastAccessed string
+	NumVisits    uint
+}
+
+func randomString(numWords int, dict []string) string {
+	// Generates a string containing numWords random words from the passed array of strings
+	// This will be out url shortening scheme
+	// E.g. server.com/HelloTestBeautifulScheme
+
+}
+
+func newShortUrl(url string) {
+
+}
